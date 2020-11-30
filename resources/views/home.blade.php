1ca1333afae81b14db4f0306cbab878c21cbcefa
Faustino Vasquez Limon <vasquez.limon@gmail.com>
2020-11-29 22:12:38 -0800

    adding template to auth home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,11 @@
-@extends('layouts.app')
+@extends('layouts.master', ['activePage' => 'dashboard', 'titlePage' => env('APP_NAME')])
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/layouts/footers/auth.blade.php
+++ resources/views/layouts/footers/auth.blade.php
@@ -3,11 +3,6 @@
     <nav class="float-left">
       <ul>
         <li>
-          <a href="https://www.creative-tim.com">
-              {{ __('Creative Tim') }}
-          </a>
-        </li>
-        <li>
           <a href="https://creative-tim.com/presentation">
               {{ __('About Us') }}
           </a>
@@ -17,19 +12,13 @@
               {{ __('Blog') }}
           </a>
         </li>
-        <li>
-          <a href="https://www.creative-tim.com/license">
-              {{ __('Licenses') }}
-          </a>
-        </li>
       </ul>
     </nav>
     <div class="copyright float-right">
       &copy;
       <script>
         document.write(new Date().getFullYear())
-      </script>, made with <i class="material-icons">favorite</i> by
-      <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a> and <a href="https://www.updivision.com" target="_blank">UPDIVISION</a> for a better web.
+      </script>, made by<a href="https://proadh.org.mx" target="_blank"></a> proadh.org.mx
     </div>
   </div>
 </footer>
